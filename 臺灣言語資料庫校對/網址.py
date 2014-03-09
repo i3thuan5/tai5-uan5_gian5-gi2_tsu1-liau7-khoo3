7a8fb857fe78fcaad2567fc685f26f7a9998acc0
Ihc <Ihc@gmail.com>
2014-03-09 23:12:04 +0800

    加轉標準，佮共資料掠來改　丞宏

--- 臺灣言語資料庫校對/網址.py
+++ 臺灣言語資料庫校對/網址.py
@@ -3,9 +3,11 @@ from django.conf.urls import patterns, url
 from 臺灣言語資料庫校對.介面 import 最近改的資料
 from 臺灣言語資料庫校對.介面 import 改愛改的資料
 from 臺灣言語資料庫校對.介面 import 檢查猶未標的資料
+from 臺灣言語資料庫校對.介面 import 定教育部辭典做標準
 
 urlpatterns = patterns('',
 	url(r'^改愛改的資料$', 改愛改的資料, name='改愛改的資料'),
 	url(r'^檢查猶未標的資料$', 檢查猶未標的資料, name='檢查猶未標的資料'),
+	url(r'^定教育部辭典做標準$', 定教育部辭典做標準, name='定教育部辭典做標準'),
 	url(r'^.*$', 最近改的資料, name='最近改的資料'),
 )
--- 臺灣言語資料庫校對/資料分類.py
+++ 臺灣言語資料庫校對/資料分類.py
@@ -3,18 +3,23 @@ from 臺灣言語資料庫.模型 import 編修
 from 臺灣言語資料庫.腔口資訊 import 閩南語
 from 臺灣言語資料庫.欄位資訊 import 愛改
 from 臺灣言語資料庫.欄位資訊 import 猶未檢查
+from 臺灣言語資料庫.欄位資訊 import 標準
 
 class 資料分類:
-	def 揣出檢查字音的資料(self, 腔, 來源):
+	def 揣出指定來源準備做檔準(self, 腔, 來源):
 		if len(來源) == 0:
 			return []
-		公家 = 編修.objects.filter(狀況 = 猶未檢查,結果__isnull=True).\
+		公家 = 編修.objects.filter(狀況 = 猶未檢查, 結果__isnull = True).\
 			filter(文字__腔口__startswith = 腔).filter(文字__組合 = '')
 		要求 = Q(文字__來源 = 來源[0])
 		for 源 in 來源[1:]:
 			要求 = 要求 | Q(文字__來源 = 源)
 		標準資料 = 公家.filter(要求)
-		愛檢查的資料 = 公家.filter(~要求)
+		return 標準資料
+	def 揣出檢查字音的資料(self, 腔):
+		標準資料 = 編修.objects.filter(狀況 = 標準, 結果__isnull = True)
+		愛檢查的資料 = 編修.objects.filter(狀況 = 猶未檢查, 結果__isnull = True).\
+			filter(文字__腔口__startswith = 腔).filter(文字__組合 = '')
 		return (標準資料, 愛檢查的資料)
 	def 揣出愛改的資料(self):
 		return 編修.objects.filter(種類 = '文字', 狀況 = 愛改)
