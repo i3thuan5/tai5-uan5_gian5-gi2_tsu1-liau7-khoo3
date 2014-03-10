7a660dfff94bb6315a3b494abb1145866d52c2ae
Ihc <Ihc@gmail.com>
2014-03-10 08:45:29 +0800

    斷詞剖析愛另外服務，速度才會緊　丞宏

---
+++ 臺灣言語資料庫斷詞標音/揣辭典條目.py
@@ -0,0 +1,48 @@
+"""
+著作權所有 (C) 民國102年 意傳文化科技
+開發者：薛丞宏
+網址：http://意傳.台灣
+語料來源：請看各資料庫內說明
+
+本程式乃自由軟體，您必須遵照SocialCalc設計的通用公共授權（Common Public Attribution License, CPAL)來修改和重新發佈這一程式，詳情請參閱條文。授權大略如下，若有歧異，以授權原文為主：
+	１．得使用、修改、複製並發佈此程式碼，且必須以通用公共授權發行；
+	２．任何以程式碼衍生的執行檔或網路服務，必須公開該程式碼；
+	３．將此程式的原始碼當函式庫引用入商業軟體，且不需公開非關此函式庫的任何程式碼
+
+此開放原始碼、共享軟體或說明文件之使用或散佈不負擔保責任，並拒絕負擔因使用上述軟體或說明文件所致任何及一切賠償責任或損害。
+
+臺灣言語工具緣起於本土文化推廣與傳承，非常歡迎各界用於商業軟體，但希望在使用之餘，能夠提供建議、錯誤回報或修補，回饋給這塊土地。
+
+感謝您的使用與推廣～～勞力！承蒙！
+"""
+from django.db.models import Q
+from 臺灣言語資料庫.模型 import 編修
+from 臺灣言語資料庫.腔口資訊 import 閩南語
+from 臺灣言語資料庫.欄位資訊 import 愛改
+from 臺灣言語資料庫.欄位資訊 import 猶未檢查
+from 臺灣言語資料庫.欄位資訊 import 標準
+from 臺灣言語資料庫.模型 import 文字
+from 臺灣言語資料庫.欄位資訊 import 語句
+from 臺灣言語資料庫.欄位資訊 import 文字組合符號
+from 臺灣言語資料庫.腔口資訊 import 國語
+from 臺灣言語資料庫.欄位資訊 import 無仝言語層
+from 臺灣言語資料庫.模型 import 關係
+from 臺灣言語資料庫.欄位資訊 import 字詞
+
+class 揣辭典條目():
+	def 揣言語層的字詞(self, 語言層):
+		資料 = 關係.objects.values_list('乙流水號').distinct()\
+			.filter(乙對甲的關係類型 = 無仝言語層, 關係性質 = 語言層).\
+			order_by('乙流水號')
+		流水號集 = []
+		for 文字 in 資料:
+			流水號集.append(資料.流水號.流水號)
+		return 流水號集
+	def 揣腔口資料(self, 腔口):
+# 	文字.objects.values_list('型體', '音標')\
+		return 編修.objects.filter(狀況 = 標準, 結果__isnull = True)\
+			.filter(文字__腔口__contains = 腔口)
+	def 揣腔口字詞資料(self, 腔口):
+# 		return 文字.objects.values_list('型體', '音標')\
+		return 編修.objects.filter(狀況 = 標準, 結果__isnull = True)\
+			.filter(腔口__contains = 腔口, 種類 = 字詞)
---
+++ 臺灣言語資料庫斷詞標音/斷詞剖析.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.views import generic
+from django.shortcuts import get_object_or_404
+from django.http.response import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.db.models import Q
+from 臺灣言語資料庫.模型 import 編修
+from 臺灣言語資料庫.模型 import 文字
+from 臺灣言語資料庫.模型 import 關係
+from 臺灣言語資料庫.模型 import 演化
+from 臺灣言語資料庫校對.資料分類 import 資料分類
+from 臺灣言語資料庫.腔口資訊 import 閩南語
+from itertools import chain
+from 臺灣言語工具.斷詞.型音辭典 import 型音辭典
+from 臺灣言語工具.字詞組集句章.解析整理.拆文分析器 import 拆文分析器
+from 臺灣言語工具.字詞組集句章.解析整理.字物件篩仔 import 字物件篩仔
+from 臺灣言語資料庫.欄位資訊 import 免改
+from 臺灣言語資料庫.欄位資訊 import 愛改
+from 臺灣言語工具.字詞組集句章.基本元素.公用變數 import 標點符號
+from 臺灣言語資料庫.欄位資訊 import 標準
+from 臺灣言語資料庫.欄位資訊 import 免檢查
+import Pyro4
+from 臺灣言語資料庫斷詞標音.閩南語標音整合 import 閩南語標音整合
+
+__資料分類 = 資料分類()
+def 斷詞剖析():
+	Pyro4.config.SERIALIZERS_ACCEPTED.add('pickle')
+	閩南語標音整合 = 閩南語標音整合(閩南語,型音辭典)
+	Pyro4.Daemon.serveSimple(
+	{
+		閩南語標音整合: "內部自動標音"
+	}, ns = True)
+# 	標準資料, 愛檢查的資料 = __資料分類.揣出檢查字音的資料(閩南語)
+# 	分析器 = 拆文分析器()
+# 	篩仔 = 字物件篩仔()
+# 	辭典 = 型音辭典(4)
+# 	for 標準 in 標準資料[:]:
+# 		文 = 標準.文字.first()
+# 		組物件 = 分析器.產生對齊組(文.型體, 文.音標)
+# 		for 詞物件 in 組物件.內底詞:
+# 			辭典.加詞(詞物件)
+# 		字陣列 = 篩仔.篩出字物件(組物件)
+# 		for 字物件 in 字陣列:
+# 			詞物件 = 分析器.建立詞物件('')
+# 			詞物件.內底字.append(字物件)
+# 			辭典.加詞(詞物件)
