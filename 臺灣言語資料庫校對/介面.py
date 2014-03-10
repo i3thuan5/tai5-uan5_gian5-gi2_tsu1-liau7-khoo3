00edca794389fc78b677efe844403cd79d0bdcfb
Ihc <ihcaoe@gmail.com>
2014-03-10 12:24:37 +0800

    欲會使振動矣 丞宏

--- 臺灣言語資料庫校對/介面.py
+++ 臺灣言語資料庫校對/介面.py
@@ -25,6 +25,8 @@ from 臺灣言語工具.字詞組集句章.基本元素.公用變數 import 標�
 from 臺灣言語資料庫.欄位資訊 import 標準
 from 臺灣言語資料庫.欄位資訊 import 免檢查
 from django.db.models import Count
+import Pyro4
+from 臺灣言語工具.字詞組集句章.解析整理.物件譀鏡 import 物件譀鏡
 
 __資料分類 = 資料分類()
 def 最近改的資料(request):
@@ -34,7 +36,7 @@ def 最近改的資料(request):
 # 	print(文字a.流水號)
 # 	關係.objects.create(甲流水號=文字a.流水號,
 # 					乙流水號=文字a.流水號,)
-	全部資料 = 編修.objects.exclude(狀況 = '正常').order_by('-修改時間')[:100]
+	全部資料 = 編修.objects.exclude(狀況='正常').order_by('-修改時間')[:100]
 	版 = loader.get_template('臺灣言語資料庫校對/最近改的資料.html')
 	文 = RequestContext(request, {
 		'全部資料': 全部資料,
@@ -42,13 +44,13 @@ def 最近改的資料(request):
 	return HttpResponse(版.render(文))
 def 定教育部辭典做標準(request):
 	來源 = ['教育部臺灣閩南語常用詞辭典', '駱嘉鵬老師對應表']
-	猶未設的標準資料 = __資料分類.揣出指定來源準備做檔準(閩南語, 來源 = 來源)
+	猶未設的標準資料 = __資料分類.揣出指定來源準備做檔準(閩南語, 來源=來源)
 	for 猶未設 in 猶未設的標準資料:
 		猶未設.狀況 = 標準
 		猶未設.save()
 	return HttpResponse("設{}做標準矣".format(來源))
 def 國語改免檢查(request):
-	國語資料= __資料分類.揣國語猶未檢查()
+	國語資料 = __資料分類.揣國語猶未檢查()
 	for 國語文字 in 國語資料:
 		國語文字.狀況 = 免檢查
 		國語文字.save()
@@ -100,15 +102,20 @@ def 檢查猶未標的資料(request):
 	return HttpResponse(版.render(文))
 def 改愛改的資料(request):
 	愛改資料 = __資料分類.揣出愛改的資料().first()
-	參考語句=__資料分類.揣出有這文字的語句(愛改資料.流水號).first()
+	參考語句 = __資料分類.揣出有這文字的語句(愛改資料.流水號).first()
+	Pyro4.config.SERIALIZER = 'pickle'
+	閩南語標音 = Pyro4.Proxy("PYRONAME:內部自動標音")
+	建議結果物件 = 閩南語標音.語句斷詞標音(參考語句)
+	譀鏡 = 物件譀鏡()
 	文 = RequestContext(request, {
 		'愛改資料': 愛改資料,
-		'參考語句':參考語句
+		'參考語句':參考語句,
+		'建議結果':譀鏡.看型(建議結果物件, 物件分詞符號=' ')
 		})
 	版 = loader.get_template('臺灣言語資料庫校對/愛改.html')
 	return HttpResponse(版.render(文))
 def 閩南語狀況(request):
-	閩南語資料 = 編修.objects.filter(文字__腔口__startswith = 閩南語)\
+	閩南語資料 = 編修.objects.filter(文字__腔口__startswith=閩南語)\
 		.values('狀況').annotate(數量=Count('狀況')).order_by()
 	文 = RequestContext(request, {
 		'閩南語': 閩南語資料,
