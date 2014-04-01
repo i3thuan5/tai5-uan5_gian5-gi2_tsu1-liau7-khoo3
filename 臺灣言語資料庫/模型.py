1527b1e0473c63760d56dd048d4a0b23559b9e15
chhsueh <ihcaod@gmail.com>
2014-04-01 10:29:43 +0800

    加試驗 丞宏

--- 臺灣言語資料庫/模型.py
+++ 臺灣言語資料庫/模型.py
@@ -23,14 +23,22 @@ class 編修(models.Model):
 	def 對著幾个資料(self):
 		數量 = 0
 		for 項, 目 in 編修種類:
-			數量 += getattr(self, 項).count()
+			if hasattr(self, 項):
+				數量 += 1
 		return 數量
-# 		return self.文字.count() + self.關係.count() + self.演化.count()
 	def 揣上尾結果(self):
 		這馬結果=self
 		while 這馬結果.結果!=None:
 			這馬結果=這馬結果.結果
 		return 這馬結果
+	def 揣文字編修(self):
+		if self.種類=='文字':
+			return self
+		if self.種類=='關係':
+			return self.關係.甲流水號.揣文字編修()
+		if self.種類=='演化':
+			return self.演化.甲流水號.揣文字編修()
+		return None
 	def __str__(self):
 		return ' '.join([
 			str(self.流水號) , self.種類
@@ -92,10 +100,22 @@ class 文字(資料):
 	音變 = models.TextField(blank=True)
 	收錄時間 = models.DateTimeField(auto_now_add=True)
 	修改時間 = models.DateTimeField(auto_now=True)
-# 	def save(self, *args, **kwargs):
-# 		if self.pk == None:
-# 			self.流水號 = 編修.objects.create(種類 = self.__class__.__name__)
-# 		super(文字, self).save(*args, **kwargs)
+	def 提組合文字(self):
+		if self.組合.startswith('#,') and self.組合.endswith(',#'):
+			型體=[]
+			音標=[]
+			調變=[]
+			音變=[]
+			for 流水號 in self.組合.split(',')[1:-1]:
+				文字編修=編修.objects.get(流水號=流水號).揣文字編修()
+				資料=文字編修.文字.提組合文字()
+				型體.append(資料[0])
+				音標.append(資料[1])
+				調變.append(資料[2])
+				音變.append(資料[3])
+			return [' '.join(型體),' '.join(音標),
+				' '.join(調變),' '.join(音變)]
+		return [self.型體,self.音標,self.調變,self.音變]
 	def __str__(self):
 		return ' '.join([
 			str(self.流水號) , self.來源 , self.型體])
--- 臺灣言語資料庫/試驗.py
+++ 臺灣言語資料庫/試驗.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
+from 臺灣言語資料庫.模型 import 文字
+from 臺灣言語資料庫.模型 import 關係
 
 # Create your tests here.
+class 資料庫測試(TestCase):
+	def test_揣文字編修(self):
+		文字物件=文字(年代=22)
+		文字物件.save()
+		關係物件=關係.objects.create(甲流水號=文字物件.流水號,
+					乙流水號=文字物件.流水號,)
+		self.assertEqual(文字物件.流水號.揣文字編修(),文字物件.流水號)
+		self.assertEqual(關係物件.流水號.揣文字編修(),文字物件.流水號)
