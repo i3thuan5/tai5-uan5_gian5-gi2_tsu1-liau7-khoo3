9cd250d78348fe41fd1d8c3b6e2d9718bc818d17
Ihc <Ihc@gmail.com>
2014-05-21 14:02:23 +0800

    解決o轉oo的問題　丞宏

--- 臺灣言語資料庫校對/建議漢字.py
+++ 臺灣言語資料庫校對/建議漢字.py
@@ -118,7 +118,7 @@ class 建議漢字:
 					pass
 				elif 建議字陣列[參考字物件所在 + 字物件所在].音 \
 					== 字陣列[字物件所在].音.replace('o','oo'):
-					字陣列[字物件所在]=字陣列[字物件所在].音.replace('o','oo')
+					字陣列[字物件所在].音=字陣列[字物件所在].音.replace('o','oo')
 				else:
 					對著 = False
 			if 對著:
@@ -159,6 +159,4 @@ class 建議漢字:
 		文字資料.音標 = 文字資料.音標.replace('tsit8-tso2', 'tsit8-tso7')  # 一座
 		文字資料.音標 = 文字資料.音標.replace('beh4-ho7', 'beh4-hoo7')  # 欲予
 		文字資料.音標 = 文字資料.音標.replace('hu3-tsoo7', 'hu2-tsoo7')  # 輔助
-		
-
-
+		文字資料.音標 = 文字資料.音標.replace('ham3-ko2', 'ham3-koo2')  # 譀古
