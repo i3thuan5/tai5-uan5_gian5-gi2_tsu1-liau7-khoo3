e3cd6192e2c1268be29022251a4f588cc294bb65
薛丞宏 <ihcaoe@gmail.com>
2015-02-07 14:24:27 +0800

    改資料表參考關係的名，毋過閣愛查佇傳類型名初使化的用法 丞宏

--- 臺灣言語資料庫/關係模型.py
+++ 臺灣言語資料庫/關係模型.py
@@ -6,25 +6,25 @@ from 臺灣言語資料庫.資料模型 import 文本表
 from 臺灣言語資料庫.資料模型 import 聽拍表
 
 class 翻譯影音(models.Model):
-	外語 = models.ForeignKey(外語表, related_name = '+關係甲')
-	影音 = models.ForeignKey(影音表, related_name = '+關係乙')
+	外語 = models.ForeignKey(外語表, related_name = self.__class__)
+	影音 = models.ForeignKey(影音表, related_name = '+')
 
 class 翻譯文本(models.Model):
-	外語 = models.ForeignKey(外語表, related_name = '+關係甲')
-	文本 = models.ForeignKey(文本表, related_name = '+關係乙')
+	外語 = models.ForeignKey(外語表, related_name = self.__class__)
+	文本 = models.ForeignKey(文本表, related_name = '+')
 
 class 影音文本(models.Model):
-	影音 = models.ForeignKey(影音表, related_name = '+關係甲')
-	文本 = models.ForeignKey(文本表, related_name = '+關係乙')
+	影音 = models.ForeignKey(影音表, related_name = self.__class__)
+	文本 = models.ForeignKey(文本表, related_name = '+')
 
 class 影音聽拍(models.Model):
-	影音 = models.ForeignKey(影音表, related_name = '+關係甲')
-	聽拍 = models.ForeignKey(聽拍表, related_name = '+關係乙')
+	影音 = models.ForeignKey(影音表, related_name = self.__class__)
+	聽拍 = models.ForeignKey(聽拍表, related_name = '+')
 
 class 文本校對(models.Model):
-	舊文本 = models.ForeignKey(文本表, related_name = '+關係甲')
-	新文本 = models.ForeignKey(文本表, related_name = '+關係乙')
+	舊文本 = models.ForeignKey(文本表, related_name = self.__class__)
+	新文本 = models.ForeignKey(文本表, related_name = '+')
 
 class 聽拍校對(models.Model):
-	舊聽拍 = models.ForeignKey(聽拍表, related_name = '+關係甲')
-	新聽拍 = models.ForeignKey(聽拍表, related_name = '+關係乙')
+	舊聽拍 = models.ForeignKey(聽拍表, related_name = self.__class__)
+	新聽拍 = models.ForeignKey(聽拍表, related_name = '+')
