3b2b2c3c9649ef9b3c6f8865c91f0df2242d2cdd
薛丞宏 <ihcaoe@gmail.com>
2015-02-07 14:04:00 +0800

    語言腔口佮地區另外獨立一個表 丞宏

--- 臺灣言語資料庫/資料模型.py
+++ 臺灣言語資料庫/資料模型.py
@@ -2,11 +2,6 @@
 from django.db import models
 from 臺灣言語資料庫.欄位資訊 import 文字種類
 
-class 款(models.Model):
-# 	字詞 = '字詞'
-# 	語句 = '語句'
-	種類 = models.CharField(max_length=100)
-
 class 作者(models.Model):
 	作者名 = models.CharField(max_length=100)
 	出世年= models.CharField(max_length=10)
@@ -15,16 +10,29 @@ class 作者(models.Model):
 class 權(models.Model):
 # 	會使公開，袂使公開
 	版權 = models.CharField(max_length=20)
-	
+
+class 款(models.Model):
+# 	字詞 = '字詞'
+# 	語句 = '語句'
+	種類 = models.CharField(max_length=100)
+
+class 話(models.Model):
+# 	閩南語、閩南語永靖腔、客話四縣腔、泰雅seediq…
+	語言腔口 = models.CharField(max_length=50)
+
+class 所在(models.Model):
+# 	臺灣、員林、…
+	地區 = models.CharField(max_length=50)
+
 class 資料(models.Model):
-	款 = models.ForeignKey(款, related_name='全部資料')
 	收錄者 = models.ForeignKey(作者, related_name='收的資料')
 	收錄時間 = models.DateTimeField(auto_now_add=True)
 	作者 = models.ForeignKey(作者, related_name='做的資料')
-	腔口 = models.CharField(max_length=100)
-	地區 = models.CharField(max_length=100)
-	年代 = models.CharField(max_length=10)
 	權 = models.ForeignKey(權, related_name='+')
+	款 = models.ForeignKey(款, related_name='全部資料')
+	話 = models.ForeignKey(話, related_name='全部資料')
+	年 = models.CharField(max_length=10)
+	所在 = models.ForeignKey(所在, related_name='全部資料')
 	屬性 = models.TextField() #冊名,詞性,分類,…
 	class Meta:
 		abstract = True
