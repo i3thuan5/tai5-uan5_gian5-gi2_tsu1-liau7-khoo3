48d20c4e57d9e3b8077d2fde61ef68f7ecc75344
薛丞宏 <ihcaoe@gmail.com>
2015-02-07 14:13:08 +0800

    資料表的名攏加「表」，按呢較袂濫做伙用毋著 丞宏

--- 臺灣言語資料庫/資料模型.py
+++ 臺灣言語資料庫/資料模型.py
@@ -1,68 +1,67 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from 臺灣言語資料庫.欄位資訊 import 文字種類
 
-class 作者(models.Model):
+class 作者表(models.Model):
 	作者名 = models.CharField(max_length=100)
 	出世年= models.CharField(max_length=10)
 	出世地 = models.CharField(max_length=100)
 
-class 權(models.Model):
+class 權表(models.Model):
 # 	會使公開，袂使公開
 	版權 = models.CharField(max_length=20)
 
-class 款(models.Model):
+class 款表(models.Model):
 # 	字詞 = '字詞'
 # 	語句 = '語句'
 	種類 = models.CharField(max_length=100)
 
-class 話(models.Model):
+class 話表(models.Model):
 # 	閩南語、閩南語永靖腔、客話四縣腔、泰雅seediq…
 	語言腔口 = models.CharField(max_length=50)
 
-class 所在(models.Model):
+class 所在表(models.Model):
 # 	臺灣、員林、…
 	地區 = models.CharField(max_length=50)
 
-class 語料著作時間(models.Model):
+class 語料著作時間表(models.Model):
 # 	臺灣、員林、…
 	年 = models.CharField(max_length=50)
 
-class 資料(models.Model):
-	收錄者 = models.ForeignKey(作者, related_name='收的資料')
+class 資料表(models.Model):
+	收錄者 = models.ForeignKey(作者表, related_name='收的資料')
 	收錄時間 = models.DateTimeField(auto_now_add=True)
-	作者 = models.ForeignKey(作者, related_name='做的資料')
-	權 = models.ForeignKey(權, related_name='+')
-	款 = models.ForeignKey(款, related_name='全部資料')
-	話 = models.ForeignKey(話, related_name='全部資料')
-	所在 = models.ForeignKey(所在, related_name='全部資料')
-	著作時間 = models.ForeignKey(語料著作時間, related_name='全部資料')
+	作者 = models.ForeignKey(作者表, related_name='做的資料')
+	權 = models.ForeignKey(權表, related_name='+')
+	款 = models.ForeignKey(款表, related_name='全部資料')
+	話 = models.ForeignKey(話表, related_name='全部資料')
+	所在 = models.ForeignKey(所在表, related_name='全部資料')
+	著作時間 = models.ForeignKey(語料著作時間表, related_name='全部資料')
 	屬性 = models.TextField() #冊名,詞性,分類,…
 	class Meta:
 		abstract = True
 
-class 外語(資料):
+class 外語表(資料表):
 	外語資料 = models.TextField()
 	def __str__(self):
 		return self.外語資料
 
-class 文本(資料):
+class 文本表(資料表):
 	文本資料 = models.TextField()
 	def __str__(self):
 		return self.文本資料
 
-class 影音(資料):
+class 影音表(資料表):
 	原始資料 = models.FileField()
 	網頁資料 = models.FileField()
 	def __str__(self):
 		return str(self.原始資料)
 
-class 聽拍規範(models.Model):
+class 聽拍規範表(models.Model):
 	規範名 = models.CharField(max_length=20, unique=True)
 	範例 = models.TextField()
 	說明 = models.TextField()
 
-class 聽拍(資料):
+class 聽拍表(資料表):
 	聽拍資料 = models.TextField()
 	def __str__(self):
 		return self.聽拍資料
--- 臺灣言語資料庫/關係模型.py
+++ 臺灣言語資料庫/關係模型.py
@@ -1,32 +1,30 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from 臺灣言語資料庫.資料模型 import 外語
-from 臺灣言語資料庫.資料模型 import 影音
-from 臺灣言語資料庫.資料模型 import 文本
-from 臺灣言語資料庫.資料模型 import 聽拍
+from 臺灣言語資料庫.資料模型 import 外語表
+from 臺灣言語資料庫.資料模型 import 影音表
+from 臺灣言語資料庫.資料模型 import 文本表
+from 臺灣言語資料庫.資料模型 import 聽拍表
 
 class 翻譯影音(models.Model):
-	外語 = models.ForeignKey(外語, related_name = '+關係甲')
-	影音 = models.ForeignKey(影音, related_name = '+關係乙')
-	def __str__(self):
-		return 'CC'
+	外語 = models.ForeignKey(外語表, related_name = '+關係甲')
+	影音 = models.ForeignKey(影音表, related_name = '+關係乙')
+
+class 翻譯文本(models.Model):
+	外語 = models.ForeignKey(外語表, related_name = '+關係甲')
+	文本 = models.ForeignKey(文本表, related_name = '+關係乙')
+
 class 影音文本(models.Model):
-	影音 = models.ForeignKey(影音, related_name = '+關係甲')
-	文本 = models.ForeignKey(文本, related_name = '+關係乙')
-	def __str__(self):
-		return 'CC'
+	影音 = models.ForeignKey(影音表, related_name = '+關係甲')
+	文本 = models.ForeignKey(文本表, related_name = '+關係乙')
+
 class 影音聽拍(models.Model):
-	影音 = models.ForeignKey(影音, related_name = '+關係甲')
-	聽拍 = models.ForeignKey(聽拍, related_name = '+關係乙')
-	def __str__(self):
-		return 'CC'
+	影音 = models.ForeignKey(影音表, related_name = '+關係甲')
+	聽拍 = models.ForeignKey(聽拍表, related_name = '+關係乙')
+
 class 文本校對(models.Model):
-	舊文本 = models.ForeignKey(文本, related_name = '+關係甲')
-	新文本 = models.ForeignKey(文本, related_name = '+關係乙')
-	def __str__(self):
-		return 'CC'
+	舊文本 = models.ForeignKey(文本表, related_name = '+關係甲')
+	新文本 = models.ForeignKey(文本表, related_name = '+關係乙')
+
 class 聽拍校對(models.Model):
-	舊聽拍 = models.ForeignKey(聽拍, related_name = '+關係甲')
-	新聽拍 = models.ForeignKey(聽拍, related_name = '+關係乙')
-	def __str__(self):
-		return 'CC'
+	舊聽拍 = models.ForeignKey(聽拍表, related_name = '+關係甲')
+	新聽拍 = models.ForeignKey(聽拍表, related_name = '+關係乙')
