a99351dcf410c45c83d8f8c9717d04c24401ccf7
薛丞宏 <ihcaoe@gmail.com>
2015-02-10 15:33:32 +0800

    無法度誠簡單佇一開始就提著 若愛就愛用type來產生，無需要 直接用khok字串就好 丞宏

--- 臺灣言語資料庫/關係模型.py
+++ 臺灣言語資料庫/關係模型.py
@@ -6,25 +6,25 @@ from 臺灣言語資料庫.資料模型 import 文本表
 from 臺灣言語資料庫.資料模型 import 聽拍表
 
 class 翻譯影音(models.Model):
-	外語 = models.ForeignKey(外語表, related_name = self.__class__)
+	外語 = models.ForeignKey(外語表, related_name = '翻譯影音')
 	影音 = models.ForeignKey(影音表, related_name = '+')
 
 class 翻譯文本(models.Model):
-	外語 = models.ForeignKey(外語表, related_name = self.__class__)
+	外語 = models.ForeignKey(外語表, related_name = '翻譯文本')
 	文本 = models.ForeignKey(文本表, related_name = '+')
 
 class 影音文本(models.Model):
-	影音 = models.ForeignKey(影音表, related_name = self.__class__)
+	影音 = models.ForeignKey(影音表, related_name = '影音文本')
 	文本 = models.ForeignKey(文本表, related_name = '+')
 
 class 影音聽拍(models.Model):
-	影音 = models.ForeignKey(影音表, related_name = self.__class__)
+	影音 = models.ForeignKey(影音表, related_name = '影音聽拍')
 	聽拍 = models.ForeignKey(聽拍表, related_name = '+')
 
 class 文本校對(models.Model):
-	舊文本 = models.ForeignKey(文本表, related_name = self.__class__)
+	舊文本 = models.ForeignKey(文本表, related_name = '文本校對')
 	新文本 = models.ForeignKey(文本表, related_name = '+')
 
 class 聽拍校對(models.Model):
-	舊聽拍 = models.ForeignKey(聽拍表, related_name = self.__class__)
+	舊聽拍 = models.ForeignKey(聽拍表, related_name = '聽拍校對')
 	新聽拍 = models.ForeignKey(聽拍表, related_name = '+')
