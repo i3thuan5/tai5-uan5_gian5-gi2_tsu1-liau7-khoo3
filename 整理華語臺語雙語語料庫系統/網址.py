ed69b6ccfa9522b0f818c76a4c1073952d543d1e
薛丞宏 <ihcaoe@gmail.com>
2015-03-31 19:41:37 +0800

    資料另外一個專案，資料台掉 丞宏

--- 整理華語臺語雙語語料庫系統/網址.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
-from 整理華語臺語雙語語料庫系統.整合平行語料 import 來整合平行語料
-
-urlpatterns = patterns('',
-	url(r'^來整合平行語料$', 來整合平行語料, name='來整合平行語料'),
-)
