4ade4187bd1a255283b17794851de12b0c264edb
薛丞宏 <ihcaoe@gmail.com>
2015-04-13 14:26:54 +0800

    揣內容袂使影響著資料表 fix #37 丞宏

--- 臺灣言語資料庫/試驗/資料欄位/test揣來源試驗.py
+++ 臺灣言語資料庫/試驗/資料欄位/test揣來源試驗.py
@@ -2,6 +2,7 @@
 from django.test import TestCase
 from 臺灣言語資料庫.資料模型 import 來源表
 from django.core.exceptions import ObjectDoesNotExist
+from 臺灣言語資料庫.資料模型 import 來源屬性表
 
 class 揣來源試驗(TestCase):
 	def setUp(self):
@@ -51,3 +52,18 @@ class 揣來源試驗(TestCase):
 		來源表.加來源({'名':'Dr. Pigu', '職業':'學生', })
 		self.assertRaises(ObjectDoesNotExist,
 			來源表.揣來源, {'名':'Dr. Pigu', })
+
+	def test_揣無來源袂使加來源資料(self):
+		來源表.加來源({'名':'Dr. Pigu', '出世年':'1990', '出世地':'花蓮', })
+		來源數量 = 來源表.objects.all().count()
+		
+		self.assertRaises(ObjectDoesNotExist,
+			來源表.揣來源, {'名':'Dr. Pigu', '出世年':'1990', '職業':'學生', })
+		self.assertEqual(來源表.objects.all().count(), 來源數量)
+	def test_揣無來源袂使加屬性(self):
+		來源表.加來源({'名':'Dr. Pigu', '出世年':'1990', '出世地':'花蓮', })
+		來源屬性數量 = 來源屬性表.objects.all().count()
+		
+		self.assertRaises(ObjectDoesNotExist,
+			來源表.揣來源, {'名':'Dr. Pigu', '出世年':'1990', '職業':'學生', })
+		self.assertEqual(來源屬性表.objects.all().count(), 來源屬性數量)
