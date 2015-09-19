dce9b0dd6442c3d245537799d5549c614d19676e
薛丞宏 <ihcaoe@gmail.com>
2015-09-19 13:03:32 +0800

    先加相關資料組的試驗 #82

---
+++ 臺灣言語資料庫/試驗/匯入匯出/test匯入試驗.py
@@ -0,0 +1,54 @@
+from django.test.testcases import TestCase
+from os.path import dirname, abspath, join
+from unittest.mock import patch, call
+from 臺灣言語資料庫.資料模型 import 文本表
+
+
+class 匯入試驗(TestCase):
+
+    def setUp(self):
+        #         self.匯入工具 = 匯入資料庫工具()
+        self.匯入工具 = None
+
+    @patch('臺灣言語資料庫.資料模型.文本表.加資料')
+    def test_下層攏愛做著(self, 加資料mocka):
+        self.匯入工具.匯入檔案(self._提yaml資料('異用字.yaml'))
+        self.assertEqual(加資料mocka.call_count, 2)
+
+    @patch('臺灣言語資料庫.資料模型.文本表.加資料')
+    def test_文本資料(self, 加資料mocka):
+        self.匯入工具.匯入檔案(self._提yaml資料('異用字.yaml'))
+        加資料mocka.assert_has_calls(
+            call({
+                '來源': {'名': '教育部閩南語辭典'},
+                '版權': '姓名標示-禁止改作 3.0 台灣',
+                '種類': '字詞',
+                '著作年': '2015',
+                '著作所在地': '臺灣',
+                '語言腔口': '閩南語',
+                '文本資料': '蜀',
+            }),
+            call({
+                '來源': {'名': '教育部閩南語辭典'},
+                '版權': '姓名標示-禁止改作 3.0 台灣',
+                '種類': '字詞',
+                '著作年': '2015',
+                '著作所在地': '臺灣',
+                '語言腔口': '閩南語',
+                '文本資料': '落翅仔',
+            }),
+        )
+
+    @patch('臺灣言語資料庫.資料模型.文本表.校對做')
+    def test_文本資料校對數量(self, 校對做mocka):
+        self.匯入工具.匯入檔案(self._提yaml資料('異用字.yaml'))
+        self.assertEqual(校對做mocka.call_count, 3)
+
+    @patch('臺灣言語資料庫.資料模型.文本表.校對做')
+    def test_文本資料校對資料(self, 校對做mocka):
+        self.匯入工具.匯入檔案(self._提yaml資料('異用字.yaml'))
+        self.assertEqual(文本表.objects.filter(文本資料='一').count(), 1)
+        self.assertEqual(文本表.objects.filter(文本資料='落翼仔').count(), 2)
+
+    def _提yaml資料(self, 檔名):
+        return join(dirname(abspath(__file__)), '資料', 檔名)
