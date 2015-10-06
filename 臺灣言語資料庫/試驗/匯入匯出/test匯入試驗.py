fa2bbb3d73bb96d1a2ce44b24d4206190ab296dd
薛丞宏 <ihcaoe@gmail.com>
2015-10-06 20:10:39 +0800

    加相關資料組的試驗

--- 臺灣言語資料庫/試驗/匯入匯出/test匯入試驗.py
+++ 臺灣言語資料庫/試驗/匯入匯出/test匯入試驗.py
@@ -84,6 +84,19 @@ class 匯入試驗(TestCase):
             '屬性': {'音標': 'hinn7-kiann3 phua3-0khi3-0ah4 .'}
         })
 
+    def test_翻母語濟層語料(self):
+        self.匯入工具.匯入檔案(self._提yaml資料('xls整理.yaml'))
+        self.assertEqual(文本表.objects.filter(文本資料='耳鏡破去矣。').count(), 1)
+
+    def test_翻母語相關資料語料(self):
+        self.匯入工具.匯入檔案(self._提yaml資料('掃街變成來亂的.yaml'))
+        外語 = 外語表.objects.get()
+        self.assertEqual(外語.外語資料, '今天生意好嗎!?')
+        網路文本 = 外語.翻譯文本.get().文本
+        self.assertEqual(網路文本.文本資料, '今仔日生意干好!?')
+        校對文本 = 網路文本.文本校對.get().新文本
+        self.assertEqual(校對文本.文本資料, '今仔日生意敢好!?')
+
     @patch('臺灣言語資料庫.資料模型.外語表.錄母語')
     def test_影音網址錄母語(self, 錄母語mocka):
         self.匯入工具.匯入檔案(self._提yaml資料('影音檔案.yaml'))
@@ -126,18 +139,49 @@ class 匯入試驗(TestCase):
         self.匯入工具.匯入檔案(self._提yaml資料('影音檔案.yaml'), 匯入影音=False)
         self.assertEqual(文本表.objects.all().count(), 2)
 
-    def test_翻母語濟層語料(self):
-        self.匯入工具.匯入檔案(self._提yaml資料('xls整理.yaml'))
-        self.assertEqual(文本表.objects.filter(文本資料='耳鏡破去矣。').count(), 1)
+    @patch('臺灣言語資料庫.資料模型.外語表.加資料')
+    def test_全部相關資料組外語表加資料(self, 加資料mocka):
+        self.匯入工具.匯入檔案(self._提yaml資料('全部相關資料組.yaml'))
+        加資料mocka.assert_called_once_with({
+            '收錄者': 來源表.objects.get(名='系統管理者'),
+            '來源': {'名': '教育部閩南語辭典'},
+            '版權': '姓名標示-禁止改作 3.0 台灣',
+            '種類': '語句',
+            '著作年': '2015',
+            '著作所在地': '臺灣',
+            '語言腔口': '閩南語',
+            '外語語言': '華語',
+            '外語資料': '美麗',
+        })
 
-    def test_翻母語相關資料語料(self):
-        self.匯入工具.匯入檔案(self._提yaml資料('掃街變成來亂的.yaml'))
-        外語 = 外語表.objects.get()
-        self.assertEqual(外語.外語資料, '今天生意好嗎!?')
-        網路文本 = 外語.翻譯文本.get().文本
-        self.assertEqual(網路文本.文本資料, '今仔日生意干好!?')
-        校對文本 = 網路文本.文本校對.get().新文本
-        self.assertEqual(校對文本.文本資料, '今仔日生意敢好!?')
+    @patch('臺灣言語資料庫.資料模型.外語表.錄母語')
+    def test_全部相關資料組外語表錄母語(self, 錄母語mocka):
+        self.匯入工具.匯入檔案(self._提yaml資料('全部相關資料組.yaml'))
+        錄母語mocka.assert_called_once_with({
+            '收錄者': 來源表.objects.get(名='系統管理者'),
+            '來源': {'名': '教育部閩南語辭典'},
+            '版權': '姓名標示-禁止改作 3.0 台灣',
+            '種類': '語句',
+            '著作年': '2015',
+            '著作所在地': '臺灣',
+            '語言腔口': '閩南語',
+            '原始影音資料': '耳鏡破去矣。',
+            '屬性': {'音標': 'hinn7-kiann3 phua3-0khi3-0ah4 .'}
+        })
+
+    @patch('臺灣言語資料庫.資料模型.影音表.寫文本')
+    def test_全部相關資料組影音表寫文本(self, 寫文本mocka):
+        self.匯入工具.匯入檔案(self._提yaml資料('全部相關資料組.yaml'))
+        寫文本mocka.assert_called_once_with({
+            '收錄者': 來源表.objects.get(名='系統管理者'),
+            '來源': {'名': '教育部閩南語辭典'},
+            '版權': '姓名標示-禁止改作 3.0 台灣',
+            '種類': '語句',
+            '著作年': '2015',
+            '著作所在地': '臺灣',
+            '語言腔口': '閩南語',
+            '文本資料': '媠噹噹',
+        })
 
     def _提yaml資料(self, 檔名):
         return join(dirname(abspath(__file__)), '資料', 檔名)
