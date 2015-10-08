ecb0ed2e0fcdd4f8f5069375e600902603ecd744
薛丞宏 <ihcaoe@gmail.com>
2015-10-08 21:26:02 +0800

    加匯入資料的試驗 fix #89

--- 臺灣言語資料庫/management/commands/匯入資料.py
+++ 臺灣言語資料庫/management/commands/匯入資料.py
@@ -1,15 +1,19 @@
 from django.core.management.base import BaseCommand, CommandError
-from urllib.request import urlopen
+from urllib import request
 from 臺灣言語資料庫.匯出入 import 匯出入工具
+from os.path import basename
 
 
 class Command(BaseCommand):
     help = '匯入指定的yaml'
 
     def add_arguments(self, parser):
-        parser.add_argument('網址', nargs='+', type=str)
         parser.add_argument(
-            '--是否匯入影音', type=bool, default=True, required=False
+            '網址', nargs='+', type=str,
+            help='愛下載的yaml網址'
+        )
+        parser.add_argument(
+            '--莫匯入影音', action='store_true'
         )
 
     def handle(self, *args, **參數):
@@ -17,7 +21,7 @@ class Command(BaseCommand):
         失敗網址 = []
         for 網址 in 參數['網址']:
             try:
-                with urlopen(網址):
+                with request.urlopen(網址):
                     網址陣列.append(網址)
             except:
                 失敗網址.append(網址)
@@ -25,4 +29,5 @@ class Command(BaseCommand):
             raise CommandError('無法度下載：{}'.format('\n'.join(失敗網址)))
         匯入工具 = 匯出入工具()
         for 網址 in 網址陣列:
-            匯入工具.匯入網址(網址, 參數['是否匯入影音'])
+            匯入工具.匯入網址(網址, not 參數['莫匯入影音'])
+            print('「{}」成功匯入'.format(basename(網址)), file=self.stdout)
--- 臺灣言語資料庫/匯出入.py
+++ 臺灣言語資料庫/匯出入.py
@@ -1,5 +1,6 @@
+import io
 from os.path import dirname, join
-from urllib.request import urlopen
+from urllib import request
 
 import yaml
 
@@ -20,14 +21,15 @@ class 匯出入工具:
             self.收錄者 = 來源表.objects.get_or_create(名='系統管理者')[0]
 
     def 匯入檔案(self, 檔名, 匯入影音=True):
-        with open(檔名) as 檔案:
-            self._匯入物件(yaml.load(檔案), dirname(檔名), 匯入影音)
+        with io.open(檔名) as 檔案:
+            self._匯入物件(檔案, dirname(檔名), 匯入影音)
 
     def 匯入網址(self, 網址, 匯入影音=True):
-        with urlopen(網址) as 檔案:
-            self._匯入物件(yaml.load(檔案), None, 匯入影音)
+        with request.urlopen(網址) as 檔案:
+            self._匯入物件(檔案, None, 匯入影音)
 
-    def _匯入物件(self, 資料物件, 資料目錄, 匯入影音):
+    def _匯入物件(self, yaml檔案資料, 資料目錄, 匯入影音):
+        資料物件 = yaml.load(yaml檔案資料)
         版權表.objects.get_or_create(版權=資料物件['版權'])
         self._遞迴匯入物件(
             資料物件,
