c2ea07178a44565de1c3015243ea514233819fc1
薛丞宏 <ihcaoe@gmail.com>
2016-01-14 10:23:13 +0800

    配合新pep8程式，import攏改到上頭前

--- tai5_uan5_gian5_gi2_tsu1_liau7_khoo3/wsgi.py
+++ tai5_uan5_gian5_gi2_tsu1_liau7_khoo3/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE", "tai5_uan5_gian5_gi2_tsu1_liau7_khoo3.settings"
 )
-
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
