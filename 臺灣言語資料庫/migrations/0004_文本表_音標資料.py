55da46b7e7bad48207d5ae40046110d688550fd9
薛丞宏 <ihcaoe@gmail.com>
2016-07-27 15:29:34 +0800

    實作徙音標到音標資料

--- 臺灣言語資料庫/migrations/0004_文本表_音標資料.py
+++ 臺灣言語資料庫/migrations/0004_文本表_音標資料.py
@@ -5,6 +5,27 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 
 
+def _文本的屬性音標徙去音標資料(apps, schema_editor):
+    文本表 = apps.get_model("臺灣言語資料庫", "文本表")
+    for 文本 in 文本表.objects.filter(屬性__分類='音標'):
+        音標屬性 = 文本.屬性.get(分類='音標')
+        文本.音標資料 = 音標屬性.性質
+        文本.save()
+        文本.屬性.remove(音標屬性)
+
+
+def _文本的音標資料徙轉屬性音標(apps, schema_editor):
+    文本表 = apps.get_model("臺灣言語資料庫", "文本表")
+    for 文本 in 文本表.objects.exclude(音標資料=''):
+        try:
+            音標欄位 = 文本.屬性.get(分類='音標')
+            文本.屬性.remove(音標欄位)
+        except:
+            pass
+        文本.屬性.get_or_create(分類='音標', 性質=文本.音標資料)
+        文本.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -16,5 +37,14 @@ class Migration(migrations.Migration):
             model_name='文本表',
             name='音標資料',
             field=models.TextField(blank=True, default=''),
+        ),
+        migrations.RunPython(
+            _文本的屬性音標徙去音標資料,
+            _文本的音標資料徙轉屬性音標,
+        ),
+        migrations.AlterField(
+            model_name='文本表',
+            name='音標資料',
+            field=models.TextField(blank=True),
         ),
     ]
