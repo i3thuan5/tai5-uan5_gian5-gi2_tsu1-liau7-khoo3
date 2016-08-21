96e5379ad7fe26798e3faa3912f79350d3eb81f5
薛丞宏 <ihcaoe@gmail.com>
2016-08-21 21:04:19 +0800

    字串的屬性音標愛存去音標資料

--- 臺灣言語資料庫/試驗/加資料/test加文本資料試驗.py
+++ 臺灣言語資料庫/試驗/加資料/test加文本資料試驗.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+import json
+
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from 臺灣言語資料庫.試驗.加資料.加資料試驗 import 加資料試驗
 from 臺灣言語資料庫.資料模型 import 文本表
-from django.core.exceptions import ValidationError
 
 
 class 加文本資料試驗(TestCase, 加資料試驗):
@@ -64,6 +66,14 @@ class 加文本資料試驗(TestCase, 加資料試驗):
     def test_屬性的音標愛存去音標資料(self):
         self.句內容.update({
             '屬性': {'音標': 'i1 tsiann5 sui2 .', },
+        })
+        self.資料 = self.資料表.加資料(self.句內容)
+        self.assertEqual(self.資料.音標資料, 'i1 tsiann5 sui2 .')
+        self.assertEqual(self.資料.屬性.count(), 0)
+
+    def test_字串的屬性音標愛存去音標資料(self):
+        self.句內容.update({
+            '屬性': json.dumps({'音標': 'i1 tsiann5 sui2 .', }),
         })
         self.資料 = self.資料表.加資料(self.句內容)
         self.assertEqual(self.資料.音標資料, 'i1 tsiann5 sui2 .')
