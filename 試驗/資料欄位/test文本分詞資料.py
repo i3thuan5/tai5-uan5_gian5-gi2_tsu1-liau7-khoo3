1860440a88a3622531f0d432b1ee1b805b0567f3
薛丞宏 <ihcaoe@gmail.com>
2017-01-09 14:12:21 +0800

    用傳入來的音標工具，決定愛按怎處理減號

--- 試驗/資料欄位/test文本分詞資料.py
+++ 試驗/資料欄位/test文本分詞資料.py
@@ -3,6 +3,7 @@ from 臺灣言語資料庫.資料模型 import 文本表
 from 臺灣言語資料庫.資料模型 import 來源表
 from 臺灣言語資料庫.資料模型 import 版權表
 from 臺灣言語資料庫.欄位資訊 import 會使公開
+from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
 
 
 class 文本分詞資料(TestCase):
@@ -42,7 +43,7 @@ class 文本分詞資料(TestCase):
     def test_減號一堆嘛袂使影響拼音(self):
         self.詞內容['文本資料'] = '---呵 li2-ho2'
         文本 = 文本表.加資料(self.詞內容)
-        self.assertEqual(文本.分詞資料(), '- - - 呵 li2-ho2')
+        self.assertEqual(文本.分詞資料(臺灣閩南語羅馬字拼音), '- - - 呵 li2-ho2')
 
     def test_分詞符號換做一般pipe符號(self):
         self.詞內容['文本資料'] = '｜＝安姑＝｜＝＝表小弟'
